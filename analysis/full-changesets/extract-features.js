ff1df5da13129cc26a23bb19179051b031df6fc2
Bhargav Kowshik <bhargav@mapbox.com>
2017-04-13 16:41:33 +0530

    Adding some arrow functions and promises

--- analysis/full-changesets/extract-features.js
+++ analysis/full-changesets/extract-features.js
@@ -8,10 +8,10 @@ const argv = require('minimist')(process.argv.slice(2));
 const readline = require('readline');
 const fs = require('fs');
 const request = require('request');
-let queue = require('d3-queue').queue;
 const csv = require('csv');
 const path = require('path');
 const parser = require('real-changesets-parser');
+let queue = require('d3-queue').queue;
 
 if (!argv.changesets) {
     console.log('');
@@ -23,148 +23,112 @@ if (!argv.changesets) {
     return;
 }
 
-var header;
-function printFeatures(features, callback) {
-    var columns = [
-        'changeset_id',
-        'harmful',
-        'features_created',
-        'features_modified',
-        'features_deleted'
-    ];
-    if (!header) {
-        console.log(columns.join(','));
-        header = columns;
+function isHarmful(changesetID, osmchaChangesets) {
+    for (var osmchaChangeset of osmchaChangesets) {
+        if (osmchaChangeset[0] === changesetID) return osmchaChangeset[1];
     }
-
-    var row = [];
-    for (var i = 0; i < columns.length; i++) {
-        row.push(features[columns[i]]);
-    }
-
-    csv.stringify([row], function (error, rowAsString) {
-        if (error) return callback(error);
-        process.stdout.write(rowAsString);
-        return callback();  // Nothing to return.
-    });
-}
-
-function getNewAndOldVersion(changeset, touchedFeature) {
-    var result =  {
-        'newVersion': {},
-        'oldVersion': {}
-    };
-    var versions = [];
-    for (var i = 0; i < changeset.features.length; i++) {
-        var feature = changeset.features[i];
-        if (feature.properties.id === touchedFeature.properties.id) versions.push(feature);
-    }
-    if (versions[0].properties.version > versions[1].properties.version) {
-        result['newVersion'] = versions[0];
-        result['oldVersion'] = versions[1];
-    } else {
-        result['newVersion'] = versions[1];
-        result['oldVersion'] = versions[0];
-    }
-    return result;
 }
 
 function getFeaturesCreated(changeset) {
-    var created = [];
-    var features = changeset.features;
-    for (var i = 0; i < features.length; i++) {
-        var feature = features[i];
+    let created = [];
+    changeset.features.map(feature => {
         if (feature.properties.action === 'create') created.push(feature);
-    }
+    });
     return created;
 }
 
 function getFeaturesModified(changeset) {
-    var modified = [];
-    var seenFeatures = [];
-
-    var features = changeset.features;
-    for (var i = 0; i < features.length; i++) {
-        var feature = features[i];
-        var featureID = feature.properties.id;
-
+    let modified = [];
+    let seenFeatures = [];
+    changeset.features.map(feature => {
+        let featureID = feature.properties.id;
         if ((feature.properties.action === 'modify') && (seenFeatures.indexOf(featureID) === -1)) {
             modified.push(getNewAndOldVersion(changeset, feature));
             seenFeatures.push(featureID);
         }
-    }
+    });
     return modified;
 }
 
 function getFeaturesDeleted(changeset) {
-    var deleted = [];
-    var seenFeatures = [];
-
-    var features = changeset.features;
-    for (var i = 0; i < features.length; i++) {
-        var feature = features[i];
+    let deleted = [];
+    let seenFeatures = [];
+    changeset.features.map(feature => {
         var featureID = feature.properties.id;
 
         if ((feature.properties.action === 'delete') && (seenFeatures.indexOf(featureID) === -1)) {
             deleted.push(getNewAndOldVersion(changeset, feature));
             seenFeatures.push(featureID);
         }
-    }
+    });
     return deleted;
 }
 
-function isHarmful(changesetID) {
-    for (var i = 0; i < changesetsFromOsmcha.length; i++) {
-        var row = changesetsFromOsmcha[i];
-        if (row[0] === changesetID) return row[1];
-    }
-}
-
-var changesetsFromOsmcha = [];
-function getChangesetsFromOsmcha(callback) {
-    csv.parse(fs.readFileSync(path.join(__dirname, 'full-changeset-ids.csv')), function (error, rows) {
-        if (error) return callback(error);
-
-        changesetsFromOsmcha = rows;
-        return callback();
+function getNewAndOldVersion(changeset, touchedFeature) {
+    var versions = [];
+    changeset.features.map(feature => {
+        if (feature.properties.id === touchedFeature.properties.id) versions.push(feature);
     });
+    if (versions[0].properties.version > versions[1].properties.version) return [versions[0], versions[1]];
+    else return [versions[1], versions[0]];
 }
 
-function extractFeatures(realChangeset, callback) {
+var headerPrinted = false;
+function extractFeatures(realChangeset, osmchaChangesets, callback) {
     var changeset = parser(realChangeset);
+    var changesetID = realChangeset['metadata']['id'];
 
-    // console.log(JSON.stringify(changeset));
-    // console.log(JSON.stringify(getFeaturesModified(changeset)));
+    var header = [
+        'changeset_id',
+        'harmful',
+        'features_created',
+        'features_modified',
+        'features_deleted'
+    ];
+    if (!headerPrinted) {
+        console.log(header.join(','));
+        headerPrinted = true;
+    }
 
-    var changesetID = realChangeset['metadata']['id']
+    var features = [
+        changesetID,
+        isHarmful(changesetID, osmchaChangesets),
+        getFeaturesCreated(changeset).length,
+        getFeaturesModified(changeset).length,
+        getFeaturesDeleted(changeset).length
+    ];
 
-    var features = {
-        'changeset_id': changesetID,
-        'harmful': isHarmful(changesetID),
-        'features_created': getFeaturesCreated(changeset).length,
-        'features_modified': getFeaturesModified(changeset).length,
-        'features_deleted': getFeaturesDeleted(changeset).length,
-    };
-    printFeatures(features, function (error, result) {
-        return callback();  // Nothing to return.
+    csv.stringify([features], (error, resultsAsString) => {
+        if (error) throw error;
+        process.stdout.write(resultsAsString);
+        callback();
     });
 }
 
-const q = queue(1);
-q.defer(getChangesetsFromOsmcha);
-
-const rl = readline.createInterface({
-    input: fs.createReadStream(argv.changesets),
-    outout: process.stdout
-});
-
-rl.on('line', (line) => {
-    q.defer(extractFeatures, JSON.parse(line));
-});
-
-rl.on('close', () => {
-    q.awaitAll(function (error, result) {
-        if (error) throw error;
-        // Nothing to do here.
+function getOsmchaChangesets() {
+    return new Promise((resolve, reject) => {
+        var file = fs.readFileSync(path.join(__dirname, 'full-changeset-ids.csv'));
+        csv.parse(file, (error, rows) => {
+            if (error) return reject(error);
+            resolve(rows);
+        });
     });
-});
+}
+
+function main() {
+    getOsmchaChangesets()
+    .then(osmchaChangesets => {
+        var q = queue(1);
+        const reader = readline.createInterface({
+            input: fs.createReadStream(argv.changesets),
+        });
+        reader.on('line', (line) => q.defer(extractFeatures, JSON.parse(line), osmchaChangesets));
+        reader.on('close', () => {
+            q.awaitAll((error, results) => {
+                if (error) throw error;
+            });
+        });
+    });
+}
+
+main()
